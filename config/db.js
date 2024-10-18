9b05478142a8f1fa0c47cb178fb70af9f96edf86
parkerrappleye <parker.rappleye1@gmail.com>
2024-10-18 16:01:23 -0600

    changed env variable

--- config/db.js
+++ config/db.js
@@ -3,13 +3,13 @@ const SocksProxyAgent = require('socks-proxy-agent').SocksProxyAgent;
 
 const connectDB = async () => {
   try {
-    // Check if FIXIE_URL and MONGODB_URI are defined
-    if (!process.env.FIXIE_URL || !process.env.MONGODB_URI) {
-      throw new Error('FIXIE_URL or MONGODB_URI is not defined in the environment variables.');
+    // Check if FIXIE_SOCKS_HOST and MONGODB_URI are defined
+    if (!process.env.FIXIE_SOCKS_HOST || !process.env.MONGODB_URI) {
+      throw new Error('FIXIE_SOCKS_HOST or MONGODB_URI is not defined in the environment variables.');
     }
 
-    // Create a SOCKS proxy agent using the FIXIE_URL
-    const proxyAgent = new SocksProxyAgent(process.env.FIXIE_URL);
+    // Create a SOCKS proxy agent using the FIXIE_SOCKS_HOST
+    const proxyAgent = new SocksProxyAgent(process.env.FIXIE_SOCKS_HOST);
 
     // Connect to MongoDB using mongoose without the agent option
     await mongoose.connect(process.env.MONGODB_URI, {
